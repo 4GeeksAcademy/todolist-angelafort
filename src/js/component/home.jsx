2a1cd5e5494183e5e1a265caaf52f32c9151a59f
angelafort <139291628+angelafort@users.noreply.github.com>
2023-09-28 23:21:37 +0000

    to do list con fetch listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+let urlBase = "https://playground.4geeks.com/apis/fake/todos/user/angela";
 
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
   const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const getTask = async () => {
+    try {
+      let response = await fetch(urlBase);
+      let data = await response.json();
+
+      console.log(response);
+      console.log(data);
+
+      if (response.ok) {
+        setTodos(data);
+      }
+      if (response.status === 404) {
+        createUser();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createUser = async () => {
+    try {
+      let response = await fetch(urlBase, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify([]),
+      });
+
+      if (response.ok) {
+        getTask();
+      }
+      console.log("User created successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleMouseEnter = (index) => {
     setHoveredIndex(index);
@@ -14,6 +54,49 @@ const Home = () => {
   const handleMouseLeave = () => {
     setHoveredIndex(null);
   };
+
+  const deleteTask = async (index) => {
+    const updatedTasks = todos.filter((_, currentIndex) => index !== currentIndex);
+    
+    try {
+      let response = await fetch(urlBase, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedTasks),
+      });
+      
+      if (response.ok) {
+        getTask();
+      } else {
+        console.log("Failed to delete task.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  async function deleteAll(){
+    try {
+      let response = await fetch (urlBase, {
+        method: "DELETE",
+        headers:{
+          "Content-Type": "aplication/json"
+        }
+      })
+      if (response.ok){
+        getTask()
+      }
+      console.log(error)
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(() => {
+    getTask();
+  }, []);
 
   return (
     <div className="container">
@@ -24,14 +107,27 @@ const Home = () => {
             type="text"
             onChange={(e) => setInputValue(e.target.value)}
             value={inputValue}
-            onKeyPress={(e) => {
+            onKeyPress={async (e) => {
               if (e.key === "Enter") {
-                setTodos(todos.concat([inputValue]));
-                setInputValue("");
+                try {
+                  let response = await fetch(urlBase, {
+                    method: "PUT",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify([...todos, { label: inputValue, done: false }]),
+                  });
+                  if (response.ok) {
+                    getTask();
+                    setInputValue("");
+                  }
+                } catch (error) {
+                  console.log(error);
+                }
               }
             }}
             placeholder="What needs to be done?"
-          ></input>
+          />
         </li>
         {todos.length === 0 ? (
           <li>No tasks, add a task</li>
@@ -41,22 +137,23 @@ const Home = () => {
               key={index}
               onMouseEnter={() => handleMouseEnter(index)}
               onMouseLeave={handleMouseLeave}
-            ><div className="task-list-item">
-              {item}{" "}
-              {hoveredIndex === index && (
-             <div className= "icon"> <FontAwesomeIcon
-                  icon={faTrash}
-                  onClick={() =>
-                    setTodos(
-                      todos.filter((t, currentIndex) => index !== currentIndex)
-                    )
-                  }
-                /></div>
-              )}</div>
+            >
+              <div className="task-list-item">
+                {item.label}
+                {hoveredIndex === index && (
+                  <div className="icon">
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      onClick={() => deleteTask(index)}
+                    />
+                  </div>
+                )}
+              </div>
             </li>
           ))
         )}
       </ul>
+      <button onClick={()=> deleteAll()}>Delete All</button>
       <div>{todos.length} tasks</div>
     </div>
   );
